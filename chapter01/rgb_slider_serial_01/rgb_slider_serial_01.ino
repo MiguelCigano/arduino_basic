b95b6e6a4591e40aaa645473c1191436a199bb0c
MiguelCigano <miguel_m0130@hotmail.com>
2026-02-28 12:33:05 -0800

    add code whole

--- chapter01/rgb_slider_serial_01/rgb_slider_serial_01.ino
+++ chapter01/rgb_slider_serial_01/rgb_slider_serial_01.ino
@@ -2,6 +2,9 @@ const int LED_RED    = 3;
 const int LED_GREEN  = 5;
 const int LED_BLUE   = 6;
 
+int count = 0;
+unsigned long last_msg_time = 0;
+
 void setup() {
   Serial.begin(9600);
   // Un timeout pequeño ayuda a que no se congele el loop
@@ -21,8 +24,7 @@ void loop() {
     // Si la cadena no esta vacia, extraemos los valores
     if (data.length() > 0) {
       // This line is to send data to backend.
-      Serial.println(data);
-      // Serial.println("4\n");
+      // Serial.println(data);
 
       int first_comma  = data.indexOf(',');
       int second_comma = data.indexOf(',', first_comma + 1);
@@ -38,5 +40,12 @@ void loop() {
       
       }
     }
+  }
+
+  // Mandar el numero que aumenta (Petición GET)
+  if (millis() - last_msg_time > 1000) {
+    Serial.println(count); // Esto lo leerá el hilo de lectura de Rust
+    count++;
+    last_msg_time = millis();
   }
 }
